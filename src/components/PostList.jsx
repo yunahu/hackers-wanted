1ff9ce8bb38c03014af85a3a9601e6e59c1b3b0f
Yujie Peng <ypeng24@mylangara.ca>
2024-09-22 13:03:37 -0700

    Refactor PostList styles

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,20 @@
 import PostCard from "./PostCard";
 import { useState, useEffect, useRef } from "react";
-import { useApp } from '../context/AppContext';
+import { useApp } from "../context/AppContext";
 
 function PostList() {
-  const [ loading, setLoading ] = useState(false);
+  const [loading, setLoading] = useState(false);
   const ref = useRef();
   const LIMIT = 5;
-  const { scrollPosition, setScrollPosition, postList, setPostList, offset, setOffset, selectedTags } = useApp();
+  const {
+    scrollPosition,
+    setScrollPosition,
+    postList,
+    setPostList,
+    offset,
+    setOffset,
+    selectedTags,
+  } = useApp();
 
   useEffect(() => {
     if (ref.current && scrollPosition !== 0) {
@@ -20,27 +28,27 @@ function PostList() {
       setLoading(true);
 
       try {
-        let query = `${import.meta.env.VITE_BACKEND_URL}/posts?offset=${offset}`;
+        let query = `${
+          import.meta.env.VITE_BACKEND_URL
+        }/posts?offset=${offset}`;
 
         if (selectedTags.length > 0) {
-          const tagsQuery = selectedTags.join(',');
+          const tagsQuery = selectedTags.join(",");
           query += `&tags=${tagsQuery}`;
         }
 
         console.log(query);
         const response = await fetch(query);
 
-        if (!response.ok) throw new Error('Unable to fetch posts.');
+        if (!response.ok) throw new Error("Unable to fetch posts.");
 
         const postData = await response.json();
         setPostList((prevPostList) => [...prevPostList, ...postData.posts]);
-
       } catch (err) {
         console.error(err);
       } finally {
         setLoading(false);
       }
-      
     };
 
     fetchPosts();
@@ -51,7 +59,11 @@ function PostList() {
       const scrollableDiv = ref.current;
       setScrollPosition(scrollableDiv.scrollTop);
 
-      if (scrollableDiv.scrollTop + scrollableDiv.clientHeight >= scrollableDiv.scrollHeight - 100 && !loading) {
+      if (
+        scrollableDiv.scrollTop + scrollableDiv.clientHeight >=
+          scrollableDiv.scrollHeight - 100 &&
+        !loading
+      ) {
         setOffset((prevOffset) => prevOffset + LIMIT);
       }
     }, 200);
@@ -70,14 +82,17 @@ function PostList() {
 
   function debounce(fn, delay) {
     let timeout;
-    return function(...args) {
+    return function (...args) {
       clearTimeout(timeout);
       timeout = setTimeout(() => fn.apply(this, args), delay);
     };
   }
 
   return (
-    <div ref={ref} className="flex flex-col gap-4 px-4 pt-4 bg-white max-h-[45rem] overflow-auto">
+    <div
+      ref={ref}
+      className="flex flex-col gap-4 px-4 pt-4 bg-white max-h-screen overflow-auto"
+    >
       {postList.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
