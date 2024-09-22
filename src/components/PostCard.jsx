7db44799e48041df946a13c07ebbaa5cfafd4e4f
Yuna Hu <yunaa.765@gmail.com>
2024-09-21 18:01:44 -0700

    Implement PostCard component and update PostList mock data

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,15 +1,35 @@
 function PostCard({ post }) {
   return (
-    <div className="p-2 bg-gray-100 rounded-lg">
-      PostCard
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
-      <div>
-        {post.tags.map((tag) => (
-          <span key={tag}>{tag}</span>
-        ))}
+    <div className="">
+      <div className="bg-[#d9d9d9] rounded-2xl">
+        <div className="pl-[15px] pt-[13px] pr-[26px] pb-[17px] ">
+          <div className="rounded-full bg-white flex w-[66px] items-center mb-[15px]">
+            <div className="w-2.5 h-2.5 bg-gray-300 rounded-full m-1"></div>
+            <span className="text-xs">{post.open ? "Active" : "Closed"}</span>
+          </div>
+          <h2 className="font-bold text-2xl mb-[8px]">{post.title}</h2>
+          <div className="text-white ml-[3px]">
+            <p>{post.content}</p>
+          </div>
+        </div>
+        <div className="bg-[#f5f5f5] h-9 flex gap-[20px] items-center rounded-b-2xl px-[15px]">
+          {post.tags.map((tag) => (
+            <span
+              className="bg-[#d9d9d9] px-[5px] rounded-full h-5 min-w-12	flex items-center text-xs justify-center"
+              key={tag}
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
       </div>
-      <button>Read more</button>
+      <div className="mt-[8px] ml-[15px] mr-[15px] flex justify-between">
+        <div className="flex gap-2">
+          <div className="border w-4 h-4 rounded-full border-black"></div>
+          <span className="text-xs">{post.author}</span>
+        </div>
+        <span className="text-xs">{post.date}</span>
+      </div>
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,15 +4,21 @@ import PostCard from "./PostCard";
 const posts = [
   {
     id: 1,
-    title: "Post 1",
+    title: "Post 1 title",
     content: "Post 1 content",
     tags: ["tag1", "tag2"],
+    date: "2024-09-21",
+    author: "Username",
+    open: true,
   },
   {
     id: 2,
-    title: "Post 2",
+    title: "Post 2 title",
     content: "Post 2 content",
     tags: ["tag2", "tag3"],
+    date: "2024-09-21",
+    author: "author2",
+    open: false,
   },
 ];
 
